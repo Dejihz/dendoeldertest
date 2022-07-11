7c6e0fbff888167c46e23465f6b4706b71660add
Dejihz <olag0002@hz.nl>
2022-07-11 13:00:53 +0200

    Update 2022_05_11_092416_create_orders_table.php

--- database/migrations/2022_05_11_092416_create_orders_table.php
+++ database/migrations/2022_05_11_092416_create_orders_table.php
@@ -16,11 +16,11 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('order_number');
-            $table->unsignedBigInteger('pallet_id')->unique();
-            $table->foreign('pallet_id')
-                ->references('product_id')
-                ->on('pallets')
-                ->onDelete('restrict');
+//            $table->unsignedBigInteger('pallet_id')->unique();
+//            $table->foreign('pallet_id')
+//                ->references('product_id')
+//                ->on('pallets')
+//                ->onDelete('restrict');
             $table->unsignedBigInteger('machine_id');
             $table->foreign('machine_id')
                 ->references('id')

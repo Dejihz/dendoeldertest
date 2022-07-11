b39e4955698ea376d1d174645fb99c13e9eb017b
Dejihz <olag0002@hz.nl>
2022-07-11 13:11:33 +0200

    a

--- resources/views/reports/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<h1 class="text-center" style="padding-top: 2rem"><span class="badge badge-pill badge-warning">Select a machine from the side menu</span></h1>
-
-@endsection

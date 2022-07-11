2c34be0036097b247602d5e81e005a259f7bddbf
Dejihz <olag0002@hz.nl>
2022-07-11 13:53:28 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
+//            UserSeeder::class,
             LocationSeeder::class,
             PalletSeeder::class,
             MaterialSeeder::class,
